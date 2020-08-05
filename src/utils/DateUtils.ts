90d4c950a1a96c857ef05fdc927bf4fe56bf07e8
Aléxia <lecavsalexia@gmail.com>
2020-08-04 22:07:42 -0300

    feat(DateUtils): create function to calculate user age.

---
+++ src/utils/DateUtils.ts
@@ -0,0 +1,11 @@
+export class DateUtils {
+	public static calculateAgeFromBirthDate(birthDate: Date) {
+		const today = new Date()
+		let age = today.getFullYear() - birthDate.getFullYear()
+		const m = today.getMonth() - birthDate.getMonth()
+		if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+			age--
+		}
+		return age
+	}
+}
