d1f9ef327a44e4115f1c83e6a55bfc272c8545ac
Aléxia <lecavsalexia@gmail.com>
2020-08-10 22:27:47 -0300

    feat(localStorage): create local storage utils.

---
+++ src/utils/LocalStorageUtils.ts
@@ -0,0 +1,22 @@
+const PREFIX = 'CAMP_PPJ_'
+const USER_AUTH_TOKEN = 'USER_AUTH_TOKEN'
+
+export class LocalStorageUtils {
+	private constructor() {}
+
+	public static getItem(key: string): string | null {
+		return localStorage.getItem(PREFIX + key)
+	}
+
+	public static setItem(key: string, value: any): void {
+		localStorage.setItem(PREFIX + key, JSON.stringify(value))
+	}
+
+	public static getToken(): string {
+		return this.getItem(USER_AUTH_TOKEN)!
+	}
+
+	public static setToken(token: string): void {
+		this.setItem(USER_AUTH_TOKEN, token)
+	}
+}
