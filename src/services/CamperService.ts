d951e0e3b8520c0e25d051932731002f9aa782b1
alexiadorneles <lecavsalexia@gmail.com>
2020-08-22 22:12:08 -0300

    feat(camper): add answer activity in CamperService.

--- src/services/CamperService.ts
+++ src/services/CamperService.ts
@@ -1,3 +1,4 @@
+import { CamperActivity } from '../model/Activity'
 import { Camper } from '../model/Camper'
 import { BaseCRUDService } from './BaseCRUDService'
 import { HttpService } from './HttpService'
@@ -9,5 +10,10 @@ export class CamperService extends BaseCRUDService<Camper> {
 
 	public async setCabin(idCamper: number, idCabin: number): Promise<number> {
 		return this.httpService.put(`/campers/${idCamper}/cabin`, { idCabin })
+	}
+
+	public async answerActivity(idCamper: number, answer: Partial<CamperActivity>): Promise<CamperActivity> {
+		answer.idCamper = idCamper
+		return this.httpService.post(`/campers/${idCamper}/answer`, answer)
 	}
 }
