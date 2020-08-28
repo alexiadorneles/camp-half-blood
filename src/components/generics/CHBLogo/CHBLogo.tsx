32f7e3b6799e27daf3e4dd2d65cc1dd3d0c0db15
alexiadorneles <lecavsalexia@gmail.com>
2020-08-27 23:53:52 -0300

    feat(logo): add orange option.

--- src/components/generics/CHBLogo/CHBLogo.tsx
+++ src/components/generics/CHBLogo/CHBLogo.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import logo from '../../../assets/images/favicon.png'
+import logoOrange from '../../../assets/images/favicon-orange-2.png'
 
 import './CHBLogo.scss'
 
-export function CHBLogo() {
+export interface CHBLogoPropTypes {
+	isPrimaryColor?: boolean
+}
+
+export function CHBLogo({ isPrimaryColor }: CHBLogoPropTypes) {
 	return (
 		<div className='CHBLogo'>
-			<img className='CHBLogo__image' src={logo} alt='Camp Half Blood Logo' />
+			<img className='CHBLogo__image' src={isPrimaryColor ? logoOrange : logo} alt='Camp Half Blood Logo' />
 		</div>
 	)
 }
