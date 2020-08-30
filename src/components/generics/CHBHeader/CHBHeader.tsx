282f1b93c45c4ce750ee3b926767c0f4c34af350
alexiadorneles <lecavsalexia@gmail.com>
2020-08-30 17:30:46 -0300

    feat(header): implement signout.

--- src/components/generics/CHBHeader/CHBHeader.tsx
+++ src/components/generics/CHBHeader/CHBHeader.tsx
@@ -2,8 +2,17 @@ import { AppBar, Button, Toolbar } from '@material-ui/core'
 import React from 'react'
 import { CHBLogo } from '../CHBLogo/CHBLogo'
 import './CHBHeader.scss'
+import { LocalStorageUtils } from '../../../utils'
+import { useHistory } from 'react-router'
 
 export function CHBHeader() {
+	const history = useHistory()
+	function logout() {
+		LocalStorageUtils.setItem('idCamper', null)
+		LocalStorageUtils.setToken(null!)
+		history.push('/')
+	}
+
 	return (
 		<AppBar className='CHBHeader' color='secondary' position='static'>
 			<Toolbar>
@@ -11,7 +20,9 @@ export function CHBHeader() {
 					<CHBLogo isPrimaryColor />
 					<p className='CHBHeader__title--text'> ACAMPAMENTO </p>
 				</div>
-				<Button color='primary'>Sair</Button>
+				<Button onClick={logout} color='primary'>
+					Sair
+				</Button>
 			</Toolbar>
 		</AppBar>
 	)
