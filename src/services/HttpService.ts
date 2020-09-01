c297d5794d2b4b5b1bfb56261e2ab257da8138b4
alexiadorneles <lecavsalexia@gmail.com>
2020-09-01 20:30:33 -0300

    hotfix(config): change start script.

--- src/services/HttpService.ts
+++ src/services/HttpService.ts
@@ -12,7 +12,7 @@ const CONFIG: Partial<AxiosRequestConfig> = {
 	},
 }
 
-const BASE_URL = 'http://localhost:3333'
+const BASE_URL = process.env.REACT_APP_BACKEND_URL
 
 export enum HTTPMethod {
 	GET = 'get',
