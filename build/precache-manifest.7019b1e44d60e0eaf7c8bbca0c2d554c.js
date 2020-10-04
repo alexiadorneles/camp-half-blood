39adf059cc70e81528d188f40b2404a6f4efbe4a
alexiadorneles <lecavsalexia@gmail.com>
2020-10-04 12:28:00 -0300

    desperate fix for umbler deploy :(

---
+++ build/precache-manifest.7019b1e44d60e0eaf7c8bbca0c2d554c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d6b23561ca4203630e308ce1176ee9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e72b630d7ac8c6f0a0d",
+    "url": "/static/css/main.360dbb3d.chunk.css"
+  },
+  {
+    "revision": "0464d1582ef08ac6e95e",
+    "url": "/static/js/2.1a41bd54.chunk.js"
+  },
+  {
+    "revision": "b8390ff20f3be3a8163bc3b607e0d235",
+    "url": "/static/js/2.1a41bd54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e72b630d7ac8c6f0a0d",
+    "url": "/static/js/main.26b596c1.chunk.js"
+  },
+  {
+    "revision": "1534ab8a8dbcce4fc782",
+    "url": "/static/js/runtime-main.15f39ccf.js"
+  },
+  {
+    "revision": "c351b5cb3be799236f17e8785956e51a",
+    "url": "/static/media/favicon-orange-2.c351b5cb.png"
+  },
+  {
+    "revision": "5fcd8a3eb40c33b4888b017d81a6bbf3",
+    "url": "/static/media/favicon.5fcd8a3e.png"
+  }
+]);
