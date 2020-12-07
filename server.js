6f473195f9dafd54e6e923668732ed478fec5798
alexiadorneles <lecavsalexia@gmail.com>
2020-12-07 20:48:09 -0300

    test deploy

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+const { exec } = require("child_process");
 const express = require("express");
 const path = require("path");
+
 const app = express();
 
 const configs = {
@@ -8,20 +10,22 @@ const configs = {
   port: process.env.PORT || 3000
 }
 
-if (configs.forcarHTTPS) //Se o redirecionamento HTTP estiver habilitado, registra o middleware abaixo
-  app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele
-    if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP
-      res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS
-    else //Se a requisição já é HTTPS
-      next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
+exec("npm run build", () => {
+  if (configs.forcarHTTPS) //Se o redirecionamento HTTP estiver habilitado, registra o middleware abaixo
+    app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele
+      if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP
+        res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS
+      else //Se a requisição já é HTTPS
+        next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
+    });
+
+  app.use(express.static(configs.caminho)); //Serve os outros arquivos, como CSSs, Javascripts, Imagens etc.
+
+  app.get("*", (req, res) => {// O wildcard '*' serve para servir o mesmo index.html independente do caminho especificado pelo navegador.
+    res.sendFile(path.join(__dirname, configs.caminho, "index.html"));
   });
 
-app.use(express.static(configs.caminho)); //Serve os outros arquivos, como CSSs, Javascripts, Imagens etc.
-
-app.get("*", (req, res) => {// O wildcard '*' serve para servir o mesmo index.html independente do caminho especificado pelo navegador.
-  res.sendFile(path.join(__dirname, configs.caminho, "index.html"));
-});
-
-app.listen(configs.port, () => {
-  console.log(`Escutando na ${configs.port}!`);
-});
+  app.listen(configs.port, () => {
+    console.log(`Escutando na ${configs.port}!`);
+  });
+})
