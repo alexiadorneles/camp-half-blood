0386e7c9b59a9369ce75e2f10a36af69e839b8aa
alexiadorneles <lecavsalexia@gmail.com>
2021-01-06 00:28:51 -0300

    remove extra install and build.

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const { exec } = require("child_process");
 const express = require("express");
 const path = require("path");
 
@@ -10,26 +9,20 @@ const configs = {
   port: process.env.PORT || 3000
 }
 
-console.log("Building...");
-exec("npm install && npm run build", (error, stdout, stderr) => {
-  console.error('ERROR', error)
-  console.log('out', stdout)
-  console.error('sterr', stderr)
-  if (configs.forcarHTTPS) //Se o redirecionamento HTTP estiver habilitado, registra o middleware abaixo
-    app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele
-      if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP
-        res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS
-      else //Se a requisição já é HTTPS
-        next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
-    });
-
-  app.use(express.static(configs.caminho)); //Serve os outros arquivos, como CSSs, Javascripts, Imagens etc.
-
-  app.get("*", (req, res) => {// O wildcard '*' serve para servir o mesmo index.html independente do caminho especificado pelo navegador.
-    res.sendFile(path.join(__dirname, configs.caminho, "index.html"));
+if (configs.forcarHTTPS) //Se o redirecionamento HTTP estiver habilitado, registra o middleware abaixo
+  app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele
+    if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP
+      res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS
+    else //Se a requisição já é HTTPS
+      next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
   });
 
-  app.listen(configs.port, () => {
-    console.log(`Escutando na ${configs.port}!`);
-  });
-})
+app.use(express.static(configs.caminho)); //Serve os outros arquivos, como CSSs, Javascripts, Imagens etc.
+
+app.get("*", (req, res) => {// O wildcard '*' serve para servir o mesmo index.html independente do caminho especificado pelo navegador.
+  res.sendFile(path.join(__dirname, configs.caminho, "index.html"));
+});
+
+app.listen(configs.port, () => {
+  console.log(`Escutando na ${configs.port}!`);
+});
