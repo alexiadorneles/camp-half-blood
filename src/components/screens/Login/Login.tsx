3efbe77ffa10ab0d885eccfb2eecbe3eef372602
Aléxia <alexiadorneles02@gmail.com>
2021-03-29 12:47:06 -0300

    refactor(login): remove styles unused.

--- src/components/screens/Login/Login.tsx
+++ src/components/screens/Login/Login.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@material-ui/core'
 import React from 'react'
 import { GoogleLogin, GoogleLoginResponse, GoogleLoginResponseOffline } from 'react-google-login'
 import { useHistory } from 'react-router'
@@ -47,28 +48,20 @@ export function Login({ camperService }: LoginPropTypes) {
 	}
 
 	return (
-		<div className='Login'>
-			<CHBLogo className='Login__logo' isPrimaryColor />
-			<p className='Login__welcomeText'>
+		<div className="Login">
+			<CHBLogo className="Login__logo" isPrimaryColor />
+			<p className="Login__welcomeText">
 				Bem vindo ao
 				<br />
 				Acampamento
 			</p>
-			<div className='Login__google'>
-				<GoogleLogin
-					icon={false}
-					className='Login__google--button'
-					clientId={REACT_APP_GOOGLE_KEY!}
-					buttonText='Entrar com Google'
-					onSuccess={successCallbackGoogle}
-					onFailure={errorCallbackGoogle}
-					cookiePolicy={'single_host_origin'}
-				/>
+			<div className="Login__google">
+				<Button className="Login__google--button">Entrar com Google</Button>
 			</div>
-			<div className='Login__terms'>
+			<div className="Login__terms">
 				<p>
 					Ao clicar em "Entrar" acima você concorda com os{' '}
-					<a href='https://portalpercyjackson.com/termos/' className='Login__terms-link'>
+					<a href="https://portalpercyjackson.com/termos/" className="Login__terms-link">
 						termos do aplicativo.
 					</a>
 				</p>
